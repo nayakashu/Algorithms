f7bfb305f61b945f77686a5c395b995a21420b61
routbisu <biswaranjan.rout1989@gmail.com>
2016-05-20 18:34:50 +0530

    FirstRUn

---
+++ TimeLoggerWindows/TimeLoggerWindows/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace TimeLoggerWindows
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnLogTime = new System.Windows.Forms.Button();
+            this.tmrLogTime = new System.Windows.Forms.Timer(this.components);
+            this.lblLastTime = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnLogTime
+            // 
+            this.btnLogTime.Location = new System.Drawing.Point(242, 99);
+            this.btnLogTime.Name = "btnLogTime";
+            this.btnLogTime.Size = new System.Drawing.Size(176, 45);
+            this.btnLogTime.TabIndex = 0;
+            this.btnLogTime.Text = "Start Logging";
+            this.btnLogTime.UseVisualStyleBackColor = true;
+            this.btnLogTime.Click += new System.EventHandler(this.btnLogTime_Click);
+            // 
+            // tmrLogTime
+            // 
+            this.tmrLogTime.Interval = 60000;
+            this.tmrLogTime.Tick += new System.EventHandler(this.tmrLogTime_Tick);
+            // 
+            // lblLastTime
+            // 
+            this.lblLastTime.AutoSize = true;
+            this.lblLastTime.Location = new System.Drawing.Point(28, 130);
+            this.lblLastTime.Name = "lblLastTime";
+            this.lblLastTime.Size = new System.Drawing.Size(0, 13);
+            this.lblLastTime.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(430, 156);
+            this.Controls.Add(this.lblLastTime);
+            this.Controls.Add(this.btnLogTime);
+            this.Name = "Form1";
+            this.Text = "Time Logger";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLogTime;
+        private System.Windows.Forms.Timer tmrLogTime;
+        private System.Windows.Forms.Label lblLastTime;
+    }
+}
+
---
+++ TimeLoggerWindows/TimeLoggerWindows/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace TimeLoggerWindows
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogTime_Click(object sender, EventArgs e)
+        {
+            tmrLogTime.Start();
+        }
+
+        private void tmrLogTime_Tick(object sender, EventArgs e)
+        {
+            LogTimeToDB();
+            lblLastTime.Text = DateTime.Now.ToString();
+        }
+
+        private void LogTimeToDB()
+        {
+            string connectionString = "Data Source=172.16.33.180; Initial Catalog=AccrtvTDM; User ID=sa; Password=Change123";
+            string mapAPIAddress = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=12.905166,77.629748&destinations=12.948156,77.708109&departure_time=now&key=AIzaSyBx23Lnkzn_9VBceuRpaGYqDMLavpmZOdw";
+
+            WebClient webClient = new WebClient();
+            dynamic result = JsonConvert.DeserializeObject(webClient.DownloadString(mapAPIAddress));
+            double timeTakenInTraffic = Convert.ToInt32(result.rows[0].elements[0].duration_in_traffic.value);
+            int timeTakenInMins = Convert.ToInt32(Math.Round(timeTakenInTraffic / 60));
+
+            SqlConnection connection = new SqlConnection(connectionString);
+            string insertCommandText = "INSERT INTO [dbo].[TravelTime] VALUES (1, " + timeTakenInMins + ", GETDATE());";
+
+            connection.Open();
+
+            SqlCommand insertCommand = new SqlCommand(insertCommandText, connection);
+            insertCommand.ExecuteNonQuery();
+
+            connection.Close();
+        }
+    }
+}
